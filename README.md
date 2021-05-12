ab4966d0186f0415fdfa231d28645d4c431821e9
Ryan Anderson <ryan0425@msn.com>
2021-05-11 22:59:20 -0700

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # BudgetTracker
+
+Professional README.md
+
+## Description
+
+Budget Tracker
+
+## License
+
+![ISC License](https://img.shields.io/static/v1.svg?label=License&message=MIT&color=yellow)
+
+## Installation
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running. Enter any budget items you have:
+-You can add and subtract items with titles and it will update your budget and provide a graph to help track your budget!
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+## Credits
+
+List your collaborators, if any, with links to their GitHub profiles.
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+If you followed tutorials, include links to those here as well. undefined
+
+## How to Contribute
+
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer. Ryan Anderson
+
+## Tests
+
+Provide examples or screenshots to prove working.
+
+## Questions
+
+Any questions?
+
+What is your GitHub username? @ryansswell
+
+What is your email? ryan0425@msn.com
+
+What is the name of your project? Budget Tracker
+
+What is your LinkedIn?
--- public/index.js
+++ public/index.js
@@ -6,7 +6,6 @@ fetch('/api/transaction')
     return response.json();
   })
   .then((data) => {
-    // save db data on global variable
     transactions = data;
 
     populateTotal();
@@ -15,7 +14,6 @@ fetch('/api/transaction')
   });
 
 function populateTotal() {
-  // reduce transaction amounts to a single total value
   const total = transactions.reduce((currentTotal, amount) => {
     return currentTotal + parseInt(amount.value);
   }, 0);
@@ -29,7 +27,6 @@ function populateTable() {
   tbody.innerHTML = '';
 
   transactions.forEach((transaction) => {
-    // create and populate a table row
     const tr = document.createElement('tr');
     tr.innerHTML = `
       <td>${transaction.name}</td>
@@ -41,11 +38,9 @@ function populateTable() {
 }
 
 function populateChart() {
-  // copy array and reverse it
   const reversed = transactions.slice().reverse();
   let sum = 0;
 
-  // create date labels for chart
   const labels = reversed.map((t) => {
     const date = new Date(t.date);
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
@@ -85,7 +80,6 @@ function sendTransaction(isAdding) {
   const amountEl = document.querySelector('#t-amount');
   const errorEl = document.querySelector('.form .error');
 
-  // validate form
   if (nameEl.value === '' || amountEl.value === '') {
     errorEl.textContent = 'Missing Information';
     return;
@@ -93,27 +87,22 @@ function sendTransaction(isAdding) {
 
   errorEl.textContent = '';
 
-  // create record
   const transaction = {
     name: nameEl.value,
     value: amountEl.value,
     date: new Date().toISOString(),
   };
 
-  // if subtracting funds, convert amount to negative number
   if (!isAdding) {
     transaction.value *= -1;
   }
 
-  // add to beginning of current array of data
   transactions.unshift(transaction);
 
-  // re-run logic to populate ui with new record
   populateChart();
   populateTable();
   populateTotal();
 
-  // also send to server
   fetch('/api/transaction', {
     method: 'POST',
     body: JSON.stringify(transaction),
@@ -135,16 +124,12 @@ function sendTransaction(isAdding) {
       }
     })
     .catch((err) => {
-      // save record function
-      // function saveRecord();
 
-      // fetch failed, so save in indexed db
       console.log('save record');
       saveRecord(transaction);
 
 
 
-      // clear form
       nameEl.value = '';
       amountEl.value = '';
     });
